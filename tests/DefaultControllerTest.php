2c9685a03b26a95f874e98656d36486900300aab
Ahmad Tawila <ahmad@tawila.net>
2018-04-12 17:10:58 +0200

    [WIP] Install RestAPI Bundle to expose a full Restfull interface including exceptions & errors.

--- tests/DefaultControllerTest.php
+++ tests/DefaultControllerTest.php
@@ -13,4 +13,16 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
     }
+
+    public function testSearchControllerAction()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/search?f[hotel_name]=hotel&f[city]=cairo&f[price_max]=200&s[price]=asc');
+
+        $response = $client->getResponse();
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame('application/json', $response->headers->get('content-type'));
+        $this->assertSame('cairo', json_decode($response->getContent())[0]->city);
+        $this->assertContains('hotel', json_decode($response->getContent())[0]->name, '', true);
+    }
 }
